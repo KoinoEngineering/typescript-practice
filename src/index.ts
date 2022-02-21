194f13813c2537220910451602c37291c4fcfb0c
Ozekitty_ <zyun004@gmail.com>
2022-02-21 15:21:38 +0900

    計算式を考える

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello World!")
+let i = 0
+console.log(i++)   // -> ？
+console.log(i++)  // -> ？
+console.log(++i)  // -> ？
+console.log(i--)  // -> ？
+console.log(i+=3)  // -> ？
+console.log(--i)  // -> ？
+console.log(i-=5)  // -> ？
