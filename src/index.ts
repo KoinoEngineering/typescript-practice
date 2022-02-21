6bf644b3876d4ce9b881a834b89c0aeed9a43561
Ozekitty_ <zyun004@gmail.com>
2022-02-21 16:33:29 +0900

    計算式を考える

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,17 @@ console.log(i--)  // -> ？
 console.log(i+=3)  // -> ？
 console.log(--i)  // -> ？
 console.log(i-=5)  // -> ？
+
+let x = 1234+5678
+console.log(x)
+let y = 9876+4321
+console.log(y)
+let z = 450*1.08
+console.log(z)
+
+let p = 1235
+console.log(p*10)
+console.log(p%10)
+let k = 5
+console.log(k**4)
+
