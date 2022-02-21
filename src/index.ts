a2ec7f1f15f6c869fe9441cd55cf54d4931170c7
Ozekitty_ <zyun004@gmail.com>
2022-02-21 16:54:54 +0900

    計算式を考える

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
-let i = 0
-console.log(i++)   // -> ？
-console.log(i++)  // -> ？
-console.log(++i)  // -> ？
-console.log(i--)  // -> ？
-console.log(i+=3)  // -> ？
-console.log(--i)  // -> ？
-console.log(i-=5)  // -> ？
-
 console.log(1234+5678)
 console.log(876-4321)
 console.log(450*1.08)
@@ -16,3 +7,11 @@ console.log(p%10)
 let k = 5
 console.log(k**4)
 
+let i = 0
+console.log(i++)   // -> ？
+console.log(i++)  // -> ？
+console.log(++i)  // -> ？
+console.log(i--)  // -> ？
+console.log(i+=3)  // -> ？
+console.log(--i)  // -> ？
+console.log(i-=5)  // -> ？
