f9b3ac6d5fa0b47843b227aef9b21385ffec6173
Ozekitty_ <zyun004@gmail.com>
2022-02-22 10:29:18 +0900

    繰り返しと条件分岐

--- src/index.ts
+++ src/index.ts
@@ -16,3 +16,25 @@ console.log(i--)  // -> ？
 console.log(i+=3)  // -> ？
 console.log(--i)  // -> ？
 console.log(i-=5)  // -> ？
+
+for ( let count=1; count<101; count++ ) {
+    console.log(count)
+}
+
+
+let F="Fizz"
+let B="Buzz"
+for ( let count=1; count<=100; count++ ) {
+    if(count%3==0&&count%5==0){
+        console.log(F+B)}
+    else
+    if(count%3==0){
+        console.log(F)
+    }
+    else 
+    if(count%5==0){
+        console.log(B)
+    }
+    else console.log(count)
+}
+
